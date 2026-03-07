9c67333a60dec7b6afe5a50383fd4bc5df275a0e
Lu-Wannell <2727686@students.wits.ac.za>
2026-03-07 13:37:42 +0200

    Added DOTween

--- Local-Multiplayer-Game!/Assets/Scripts/ObjectShaker.cs
+++ Local-Multiplayer-Game!/Assets/Scripts/ObjectShaker.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class ObjectShaker : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.DOShake
     }
 
     // Update is called once per frame
